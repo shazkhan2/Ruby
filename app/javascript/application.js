0d149cd1a4fe27d8416219ad0ef62c025c9bd82c
shazkhan2 <seewardak@hotmail.com>
2024-09-01 15:05:03 +0200

    Error on signout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+// app/javascript/application.js
+import { Turbo } from "@hotwired/turbo-rails"
+import { Rails } from "rails-ujs"
+
+Rails.start()
+Turbo.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,6 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "rails-ujs", to: "https://cdnjs.cloudflare.com/ajax/libs/rails-ujs/7.1.3-4/rails-ujs.min.js", preload: true
+
 pin_all_from "app/javascript/controllers", under: "controllers"
