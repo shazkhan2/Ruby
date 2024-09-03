7c02de49b9d2b8f70644a9b11d694819c90d003e
shazkhan2 <seewardak@hotmail.com>
2024-09-03 21:45:41 +0200

    Routes to set now

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,2 +1,47 @@
-class TaskController < ApplicationController
-end
+class TasksController < ApplicationController
+    before_action :authenticate_user!  
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+    def new
+      @task = current_user.tasks.build  
+    end
+  
+    def create
+      @task = current_user.tasks.build(task_params)  
+      if @task.save
+        redirect_to @task, notice: 'Task was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def edit
+    end
+  
+    def update
+      if @task.update(task_params)
+        redirect_to @task, notice: 'Task was successfully updated.'
+      else
+        render :edit
+      end
+    end
+  
+    def show
+    end
+  
+    def destroy
+      @task.destroy
+      redirect_to tasks_path, notice: 'Task has been deleted.'
+    end
+  
+    private
+  
+    def set_task
+      @task = current_user.tasks.find(params[:id])
+    end
+  
+    def task_params
+      params.require(:task).permit(:title, :description, :due_date, :status)
+    end
+  end
+  
