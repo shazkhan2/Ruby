2280e947ceb53b9631b3451fdc46de1473e10f8d
shazkhan2 <seewardak@hotmail.com>
2024-09-04 12:54:45 +0200

    url fixes needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
-  get "home/index"
   
+  get "home/index"
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  
   root "home#index"
   resources :users do
     resources :tasks
-
-end
+  end
 end
