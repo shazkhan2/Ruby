9c7ae3ff174c41378174c40f285c9add10f19d51
shazkhan2 <seewardak@hotmail.com>
2024-09-06 17:19:45 +0200

    Updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   protect_from_forgery  with: :exception
   before_action :authenticate_user!
-  
 
+  def after_sign_in_path_for(resource)
+    user_tasks_path(current_user)
+
+end
 end
